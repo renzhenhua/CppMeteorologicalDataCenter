198875bb4e7dd05eabdfe188be28d2320ebe4c93
renzhenhua <renzhenhua@2452503780@qq.com>
2022-03-03 20:48:40 +0800

    提交代码

---
+++ tools1/c/ftpgetfiles.cpp
@@ -0,0 +1,16 @@
+#include "_public.h"
+
+// 程序退出和信号2、15的处理函数。
+void EXIT(int sig);
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
+
+
+void EXIT(int sig)
+{
+    printf("程序退出，sig=%d\n\n", sig);
+}
