1911b65da7b8745daed89d0d59d6da6bde736f29
renzhenhua <2452503780@qq.com>
2022-03-05 11:38:21 +0800

    继续编写ftpgetfiles.cpp，调用ftp.nlist()方法列出服务器目录文件

--- idc1/c/start.sh
+++ idc1/c/start.sh
@@ -6,9 +6,9 @@
 #/project/tools1/bin/procctl 30 /project/tools1/bin/checkproc
 
 # 压缩数据中心后台服务程序的备份日志。
-/project/tools1/bin/procctl 300 /project/tools1/bin/gzipfiles /log/idc "*.log.20*" 0.02
+/project/tools1/bin/procctl 300 /project/tools1/bin/gzipfiles /log/idc "*.log.20*" 0.04
 # 生成用于测试的全国气象站点观测的分钟数据。
 /project/tools1/bin/procctl  60 /project/idc1/bin/crtsurfdata /project/idc/ini/stcode.ini /tmp/idc/surfdata /log/idc/crtsurfdata.log xml,json,csv
 
 # 清理原始的全国气象站点观测的分钟数据目录/tmp/idc/surfdata中的历史数据文件。
-/project/tools1/bin/procctl 300 /project/tools1/bin/deletefiles /tmp/idc/surfdata "*" 0.02
+/project/tools1/bin/procctl 300 /project/tools1/bin/deletefiles /tmp/idc/surfdata "*" 0.04
--- tools1/c/ftpgetfiles.cpp
+++ tools1/c/ftpgetfiles.cpp
@@ -3,14 +3,15 @@
 
 struct st_arg
 {
-    char host[31];        // 远程服务器的IP和端口。
-    int mode;             // 传输模式，1-被动模式，2-主动模式，缺省采用被动模式。
-    char username[31];    // 远程服务器ftp的用户名。
-    char password[31];    // 远程服务器ftp的密码。
-    char remotepath[301]; // 远程服务器存放文件的目录。
-    char localpath[301];  // 本地文件存放的目录。
-    char matchname[101];  // 待下载文件匹配的规则。
-} starg;
+    char host[31];         // 远程服务器的IP和端口。
+    int mode;              // 传输模式，1-被动模式，2-主动模式，缺省采用被动模式。
+    char username[31];     // 远程服务器ftp的用户名。
+    char password[31];     // 远程服务器ftp的密码。
+    char remotepath[301];  // 远程服务器存放文件的目录。
+    char localpath[301];   // 本地文件存放的目录。
+    char matchname[101];   // 待下载文件匹配的规则。
+    char listfilename[301];// 下载前列出服务器文件名的文件。
+ } starg;
 
 CLogFile logfile;
 
@@ -23,6 +24,9 @@ void _help();
 
 // 把xml解析到参数starg结构中
 bool _xmltoarg(const char *strxmlbuffer);
+
+// 下载文件功能的主函数。
+bool _ftpgetfiles();
 
 int main(int argc, char const *argv[])
 {
@@ -53,10 +57,36 @@ int main(int argc, char const *argv[])
         return -1;
 
     // 登录ftp服务器。
+    if (ftp.login(starg.host, starg.username, starg.password, starg.mode) == false)
+    {
+        logfile.Write("ftp.login(%s, %s, %s) failed.\n", starg.host, starg.username, starg.password);
+        return -1;
+    }
 
+    logfile.Write("ftp.login ok.\n"); // 正式运行后，可以注释这行代码。
+
+    _ftpgetfiles();
+
+    ftp.logout();
+    return 0;
+}
+
+// 下载文件功能的主函数。
+bool _ftpgetfiles()
+{
     // 进入ftp服务器存放文件的目录。
+    if (ftp.chdir(starg.remotepath) == false)
+    {
+        logfile.Write("ftp.chdir(%s) failed.\n", starg.remotepath);
+        return false;
+    }
 
     // 调用ftp.nlist()方法列出服务器目录中的文件，结果存放到本地文件中。
+    if (ftp.nlist(".", starg.listfilename) == false)
+    {
+        logfile.Write("ftp.nlist(%s) failed.\n", starg.remotepath);
+        return false;
+    }
 
     // 把ftp.nlist()方法获取到的list文件加载到容器vfilelist中。
 
@@ -65,9 +95,6 @@ int main(int argc, char const *argv[])
     // {
     //     // 调用ftp.get()方法从服务器下载文件
     // }
-
-    // ftp.logout();
-    return 0;
 }
 
 void EXIT(int sig)
@@ -80,7 +107,7 @@ void _help()
     printf("\n");
     printf("Using:/project/tools1/bin/ftpgetfiles logfilename xmlbuffer\n\n");
 
-    printf("Sample:/project/tools1/bin/procctl 30 /project/tools1/bin/ftpgetfiles /log/idc/ftpgetfiles_surfdata.log \"<host>127.0.0.1:21</host><mode>1</mode><username>ren</username><password>renpwd</password><localpath>/idcdata/surfdata</localpath><remotepath>/tmp/idc/surfdata</remotepath><matchname>SURF_ZH*.XML,SURF_ZH*.CSV</matchname>\"\n\n\n");
+    printf("Sample:/project/tools1/bin/procctl 30 /project/tools1/bin/ftpgetfiles /log/idc/ftpgetfiles_surfdata.log \"<host>127.0.0.1:21</host><mode>1</mode><username>ren</username><password>123456</password><localpath>/idcdata/surfdata</localpath><remotepath>/tmp/idc/surfdata</remotepath><matchname>SURF_ZH*.XML,SURF_ZH*.CSV</matchname><listfilename>/idcdata/ftplist/ftpgetfiles_surfdata.list</listfilename>\"\n\n\n");
 
     printf("本程序是通用的功能模块，用于把远程ftp服务器的文件下载到本地目录。\n");
     printf("logfilename是本程序运行的日志文件。\n");
@@ -92,7 +119,8 @@ void _help()
     printf("<remotepath>/tmp/idc/surfdata</remotepath> 远程服务器存放文件的目录。\n");
     printf("<localpath>/idcdata/surfdata</localpath> 本地文件存放的目录。\n");
     printf("<matchname>SURF_ZH*.XML,SURF_ZH*.CSV</matchname> 待下载文件匹配的规则。"
-           "不匹配的文件不会被下载，本字段尽可能设置精确，不建议用*匹配全部的文件。\n\n\n");
+           "不匹配的文件不会被下载，本字段尽可能设置精确，不建议用*匹配全部的文件。\n");
+    printf("<listfilename>/idcdata/ftplist/ftpgetfiles_surfdata.list</listfilename> 下载前列出服务器文件名的文件。\n\n\n");
 }
 
 // 把xml解析到参数starg结构中
@@ -143,6 +171,13 @@ bool _xmltoarg(const char *strxmlbuffer)
     if (strlen(starg.matchname) == 0)
     {
         logfile.Write("matchname is null.\n");
+        return false;
+    }
+
+    GetXMLBuffer(strxmlbuffer, "listfilename", starg.listfilename, 300); // 下载前列出服务器文件名的文件。
+    if (strlen(starg.listfilename) == 0)
+    {
+        logfile.Write("listfilename is null.\n");
         return false;
     }
 
