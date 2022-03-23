234a8eb2191bfb1a368b1326275fff222fea7d2a
renzhenhua <2452503780@qq.com>
2022-03-23 17:20:15 +0800

    备份阶段性代码到idc1/c/obtmindtodb2.cpp中

--- idc1/c/obtmindtodb.cpp
+++ idc1/c/obtmindtodb.cpp
@@ -5,8 +5,18 @@
 #include "_public.h"
 #include "_mysql.h"
 
-// 把站点参数文件中加载到vstcode容器中。
-bool LoadSTCode(const char *inifile);
+struct st_zhobtmind
+{
+    char obtid[11];     // 站点代码。
+    char ddatetime[21]; // 数据时间，精确到分钟。
+    char t[11];         // 温度，单位：0.1摄氏度。
+    char p[11];         // 气压，单位：0.1百帕。
+    char u[11];         // 相对湿度，0-100之间的值。
+    char wd[11];        // 风向，0-360之间的值。
+    char wf[11];        // 风速：单位0.1m/s。
+    char r[11];         // 降雨量：0.1mm。
+    char vis[11];       // 能见度：0.1米。
+} stzhobtmind;
 
 CLogFile logfile;
 
@@ -60,19 +70,6 @@ int main(int argc, char *argv[])
     // 业务处理主函数。
     _obtmindtodb(argv[1], argv[2], argv[3]);
 
-    /*
-    // 连接数据库。
-    if (conn.connecttodb(argv[2],argv[3])!=0)
-    {
-      logfile.Write("connect database(%s) failed.\n%s\n",argv[2],conn.m_cda.message); return -1;
-    }
-
-    logfile.Write("connect database(%s) ok.\n",argv[2]);
-
-    // 提交事务。
-    conn.commit();
-    */
-
     return 0;
 }
 
@@ -88,6 +85,8 @@ void EXIT(int sig)
 // 业务处理主函数。
 bool _obtmindtodb(char *pathname, char *connstr, char *charset)
 {
+    sqlstatement stmt;
+
     CDir Dir;
 
     // 打开目录。
@@ -97,25 +96,102 @@ bool _obtmindtodb(char *pathname, char *connstr, char *charset)
         return false;
     }
 
+    CFile File;
+
     while (true)
     {
         // 读取目录，得到一个数据文件名。
         if (Dir.ReadDir() == false)
             break;
 
-        logfile.Write("filename=%s\n", Dir.m_FullFileName);
-        // 这个程序的流程稍微复杂一点，所以我又采用了按部就搬的编程方法，一步一步的实现功能。
-        // 现在达到了一个小目标，把程序先做一个备份。
-        // 打开文件。
+        // 连接数据库。
+        if (conn.m_state == 0)
+        {
+            if (conn.connecttodb(connstr, charset) != 0)
+            {
+                logfile.Write("connect database(%s) failed.\n%s\n", connstr, conn.m_cda.message);
+                return -1;
+            }
 
-        /*
+            logfile.Write("connect database(%s) ok.\n", connstr);
+        }
+
+        if (stmt.m_state == 0)
+        {
+            stmt.connect(&conn);
+            stmt.prepare("insert into T_ZHOBTMIND(obtid,ddatetime,t,p,u,wd,wf,r,vis) values(:1,str_to_date(:2,'%%Y%%m%%d%%H%%i%%s'),:3,:4,:5,:6,:7,:8,:9)");
+            stmt.bindin(1, stzhobtmind.obtid, 10);
+            stmt.bindin(2, stzhobtmind.ddatetime, 14);
+            stmt.bindin(3, stzhobtmind.t, 10);
+            stmt.bindin(4, stzhobtmind.p, 10);
+            stmt.bindin(5, stzhobtmind.u, 10);
+            stmt.bindin(6, stzhobtmind.wd, 10);
+            stmt.bindin(7, stzhobtmind.wf, 10);
+            stmt.bindin(8, stzhobtmind.r, 10);
+            stmt.bindin(9, stzhobtmind.vis, 10);
+        }
+
+        logfile.Write("filename=%s\n", Dir.m_FullFileName);
+
+        // 打开文件。
+        if (File.Open(Dir.m_FullFileName, "r") == false)
+        {
+            logfile.Write("File.Open(%s) failed.\n", Dir.m_FullFileName);
+            return false;
+        }
+
+        char strBuffer[1001]; // 存放从文件中读取的一行。
+
         while (true)
         {
-          // 处理文件中的每一行。
-        }
-        */
+            if (File.FFGETS(strBuffer, 1000, "<endl/>") == false)
+                break;
+            logfile.Write("strBuffer=%s", strBuffer);
 
+            // 处理文件中的每一行。
+            memset(&stzhobtmind, 0, sizeof(struct st_zhobtmind));
+            GetXMLBuffer(strBuffer, "obtid", stzhobtmind.obtid, 10);
+            GetXMLBuffer(strBuffer, "ddatetime", stzhobtmind.ddatetime, 14);
+            char tmp[11];
+            GetXMLBuffer(strBuffer, "t", tmp, 10);
+            if (strlen(tmp) > 0)
+                snprintf(stzhobtmind.t, 10, "%d", (int)(atof(tmp) * 10));
+            GetXMLBuffer(strBuffer, "p", tmp, 10);
+            if (strlen(tmp) > 0)
+                snprintf(stzhobtmind.p, 10, "%d", (int)(atof(tmp) * 10));
+            GetXMLBuffer(strBuffer, "u", stzhobtmind.u, 10);
+            GetXMLBuffer(strBuffer, "wd", stzhobtmind.wd, 10);
+            GetXMLBuffer(strBuffer, "wf", tmp, 10);
+            if (strlen(tmp) > 0)
+                snprintf(stzhobtmind.wf, 10, "%d", (int)(atof(tmp) * 10));
+            GetXMLBuffer(strBuffer, "r", tmp, 10);
+            if (strlen(tmp) > 0)
+                snprintf(stzhobtmind.r, 10, "%d", (int)(atof(tmp) * 10));
+            GetXMLBuffer(strBuffer, "vis", tmp, 10);
+            if (strlen(tmp) > 0)
+                snprintf(stzhobtmind.vis, 10, "%d", (int)(atof(tmp) * 10));
+
+            logfile.Write("obtid=%s,ddatetime=%s,t=%s,p=%s,u=%s,wd=%s,wf=%s,r=%s,vis=%s\n", stzhobtmind.obtid, stzhobtmind.ddatetime, stzhobtmind.t, stzhobtmind.p, stzhobtmind.u, stzhobtmind.wd, stzhobtmind.wf, stzhobtmind.r, stzhobtmind.vis);
+
+            // 把结构体中的数据插入表中。
+            if (stmt.execute() != 0)
+            {
+                // 1、失败的情况有哪些？是否全部的失败都要写日志？
+                // 答：失败的原因主要有二：一是记录重复，二是数据内容非法。
+                // 2、如果失败了怎么办？程序是否需要继续？是否rollback？是否返回false？
+                // 答：如果失败的原因是数据内容非法，记录日志后继续；如果是记录重复，不必记录日志，且继续。
+                if (stmt.m_cda.rc != 1062)
+                {
+                    logfile.Write("Buffer=%s\n", strBuffer);
+                    logfile.Write("stmt.execute() failed.\n%s\n%s\n", stmt.m_sql, stmt.m_cda.message);
+                }
+            }
+        }
+        
         // 删除文件、提交事务。
+        // File.CloseAndRemove();
+
+        conn.commit();
     }
 
     return true;
